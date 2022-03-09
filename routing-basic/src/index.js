f81787f622705c4df386678b032a8343f4156150
Abhishek Goenka <abhishek.goenka@epicor.com>
2022-03-10 05:25:03 +0530

    routing-basic

---
+++ routing-basic/src/index.js
@@ -0,0 +1,12 @@
+// Readers: To prepare this project to build along in `./App.js`, complete
+// steps 1 & 2 below
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+
+import "./index.css";
+import "./App.css";
+
+import "./semantic-dist/semantic.min.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
